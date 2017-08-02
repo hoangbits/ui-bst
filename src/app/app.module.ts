2850a498cf30ae3c73a9ac5f6828a86435a403a3
Nguyen Tien Thanh <tienthanh.nguyen@tanchonggroup.com>
2017-08-02 16:02:20 +0700

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { SecureComponent } from './layout/secure';
-import { HomeComponent } from './secure/home';
+import { ScopeComponent } from './secure/scope';
+import { UserComponent } from './secure/user';
+import { RoleComponent } from './secure/role';
+import { CompanyComponent } from './secure/company';
 
 @NgModule({
   declarations: [
     AppComponent,
     SecureComponent,
-    HomeComponent,
+    ScopeComponent,
+    UserComponent,
+    RoleComponent,
+    CompanyComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/layout/secure/secure.routes.ts
+++ src/app/layout/secure/secure.routes.ts
@@ -1,6 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './../../secure/home';
+import { CompanyComponent } from './../../secure/company';
+import { UserComponent } from './../../secure/user';
+import { RoleComponent } from './../../secure/role';
+import { ScopeComponent } from './../../secure/scope';
 
 export const SECURE_ROUTES:Routes = [
-  {path: 'home', component: HomeComponent},
+    {path: 'company', component: CompanyComponent},
+    {path: 'user', component: UserComponent},
+    {path: 'role', component: RoleComponent},
+    {path: 'scope', component: ScopeComponent},
 ];
