854d03dc346d20029785b0859b75db61af916a0b
Pham Cong Uyen <conguyen.pham@tanchonggroup.com>
2017-08-10 11:18:39 +0700

    User: Manage user - Add id to each input,button

--- src/app/layout/secure/secure.routes.ts
+++ src/app/layout/secure/secure.routes.ts
@@ -1,12 +1,13 @@
 import {Routes} from '@angular/router';
 import {CompanyComponent} from './../../secure/company';
 import {UserComponent} from './../../secure/user';
-import {RoleComponent} from './../../secure/role';
+import {ListRoleComponent} from '../../secure/role/list-role/list-role.component';
+import {ListUserComponent} from '../../secure/user/list-user/list-user.component';
 import {ScopeComponent} from './../../secure/scope';
 
 export const SECURE_ROUTES: Routes = [
     {path: 'company', component: CompanyComponent},
-    {path: 'user', component: UserComponent},
-    {path: 'role', component: RoleComponent},
+    {path: 'user', component: ListUserComponent},
+    {path: 'role', component: ListRoleComponent},
     {path: 'scope', component: ScopeComponent},
 ];
