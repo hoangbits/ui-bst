b6b28399aff534f57173addad117e9184e56b53f
Nguyen Tien Thanh <tienthanh.nguyen@tanchonggroup.com>
2017-08-23 17:45:19 +0700

    implement view company as company admin

--- src/app/secure/company/company.module.ts
+++ src/app/secure/company/company.module.ts
@@ -3,8 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DataTableModule} from "angular2-datatable";
 import {PaginationModule, TooltipModule} from 'ngx-bootstrap';
-import {MdProgressSpinnerModule} from '@angular/material';
-
 import { CompanyComponent, CompanyModalEditComponent, CompanyModalViewComponent, CompanyService } from './index';
 
 @NgModule({
@@ -20,7 +18,6 @@ import { CompanyComponent, CompanyModalEditComponent, CompanyModalViewComponent,
 		PaginationModule,
 		TooltipModule.forRoot(),
 		ReactiveFormsModule,
-		MdProgressSpinnerModule
 	],
 	providers:[CompanyService],
 	entryComponents: [
